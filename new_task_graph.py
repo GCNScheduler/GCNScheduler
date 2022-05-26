94fb5a8ec2736d72cdd02cbf8dfb4d9c0e31e334
GCNScheduler-admin <GCNScheduler@gmail.com>
2022-05-26 13:34:21 -0700

    first commit

---
+++ new_task_graph.py
@@ -0,0 +1,118 @@
+from dataclasses import dataclass
+from typing import Any, Dict, List, Tuple
+
+from wfcommons.wfchef.wfchef_abstract_recipe import WfChefWorkflowRecipe
+from wfcommons.wfchef.recipes.cycles.recipe import CyclesRecipe
+from wfcommons.wfchef.recipes.montage import MontageRecipe
+from wfcommons.wfchef.recipes.seismology import SeismologyRecipe
+from wfcommons.wfchef.recipes.blast import BlastRecipe
+from wfcommons.wfchef.recipes.bwa import BwaRecipe
+from wfcommons.wfchef.recipes.epigenomics import EpigenomicsRecipe
+from wfcommons.wfchef.recipes.srasearch import SrasearchRecipe
+from wfcommons.wfchef.recipes.genome import GenomeRecipe
+from wfcommons.wfchef.recipes.soykb import SoykbRecipe
+from wfcommons.common.workflow import Workflow
+import pathlib
+import json
+import numpy as np
+import tempfile
+import torch
+
+RECIPES: Dict[str, WfChefWorkflowRecipe] = {
+    "montage": MontageRecipe,
+    "cycles": CyclesRecipe,
+    "seismology": SeismologyRecipe,
+    "blast": BlastRecipe,
+    "bwa": BwaRecipe,
+    "epigenomics": EpigenomicsRecipe,
+    "srasearch": SrasearchRecipe,
+    "genome": GenomeRecipe,
+    "soykb": SoykbRecipe
+}
+
+thisdir = pathlib.Path(__file__).resolve().parent
+
+@dataclass
+class Task():
+    name: str
+    children: List[str]
+    runtime: float
+    outputs: List[Tuple[str, int]]
+    inputs: List[Tuple[str, int]]
+    task_id: int
+
+def workflow_to_forward_graph(workflow: List[Dict[str, Any]]) -> Tuple[Dict[int, List[int]], torch.Tensor, Dict[Tuple[int, int], int]]:
+    output_providers: Dict[str, Task] = {}
+    tasks: Dict[str, Task] = {}
+    for i, task_data in enumerate(workflow):
+        task = Task(
+            name=task_data["name"],
+            children=task_data["children"],
+            runtime=task_data["runtime"],
+            outputs= [
+                (f["name"], f["size"]) 
+                for f in task_data["files"] 
+                if f["link"] == "output"
+            ],
+            inputs=[
+                (f["name"], f["size"]) 
+                for f in task_data["files"] 
+                if f["link"] == "input"
+            ],
+            task_id=i
+        )
+        tasks[task.name] = task
+        for output_name, _ in task.outputs:
+            output_providers[output_name] = task
+
+    comp = np.zeros(len(tasks))
+    edge_weights = {}
+    forward_graph: Dict[int, List[int]] = {}
+    for _, task in tasks.items():
+        comp[task.task_id] = task.runtime
+        forward_graph[task.task_id] = [
+            tasks[child_name].task_id for child_name in task.children
+        ]
+        for input_name, input_size in task.inputs:
+            if input_name not in output_providers:
+                continue
+            parent_task = output_providers[input_name]
+            edge = (parent_task.task_id, task.task_id)
+            edge_weights.setdefault(edge, 0)
+            edge_weights[edge] += input_size
+    ###
+    set_ending,set_begining = set(),set()
+    for k,v in forward_graph.items():
+        set_ending.add(k)
+        for value in v:
+            set_ending.add(value)
+    print("DESTINATIONS",list(set_ending.difference(set_begining)) 
+    for dest in list(set_ending.difference(set_begining)):
+        forward_graph[new_key]=[]
+    ##
+    comp_torch = torch.Tensor([comp.tolist()])
+    return forward_graph, comp_torch, edge_weights
+
+def get_graph(recipe: str, num_tasks: int) -> Dict[int, List[int]]:
+    if recipe not in RECIPES:
+        raise ValueError(f"Invalid recipe '{recipe}'. Valid recipes are: {list(RECIPES.keys())}")
+
+
+    abstract_workflow = RECIPES[recipe].from_num_tasks(num_tasks=num_tasks).build_workflow()
+    with tempfile.NamedTemporaryFile() as tf:
+        abstract_workflow.write_json(tf.name)
+        workflow_instance = json.loads(pathlib.Path(tf.name).read_text())
+    print("->get graph",workflow_to_forward_graph(workflow_instance["workflow"]["tasks"]))    
+    return workflow_to_forward_graph(workflow_instance["workflow"]["tasks"])
+    
+if __name__ == "__main__":
+    forward_graph, comp, edge_weights = get_graph("montage", 50)
+
+    # Forward Graph dictionary parents -> [child_1, child_2, ...]
+    print(forward_graph)
+
+    # Comp Tensor
+    print(comp)
+
+    # Edge weights (amount of data sent across task dependecies (parent, child) -> # bytes
+    print(edge_weights)
---
+++ scheduler.py
@@ -0,0 +1,349 @@
+from asyncio import Future
+from functools import partial
+from pprint import pformat
+from re import I
+from threading import Thread
+import time
+import traceback
+from typing import Dict, List, Tuple
+
+import torch
+
+
+#import rclpy
+#from rclpy.node import Node, Client, Publisher
+#from std_msgs.msg import Float64, String
+#from sensor_msgs.msg import Image
+
+#from interfaces.srv import Executor
+import json
+from uuid import uuid4
+from itertools import product
+
+#from gcnsched.ready_to_use import find_schedule
+from task_graph import TaskGraph, get_graph
+
+from copy import deepcopy
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from matplotlib import cm
+#from cv_bridge import CvBridge
+
+BYTES_SENT = 100
+
+
+class Scheduler():
+    def __init__(self,
+                 nodes: List[str],
+                 graph: TaskGraph,
+                 interval: int) -> None:
+        super().__init__('scheduler')
+        #getting parameters from the launch file
+        self.declare_parameter('nodes', [])
+        self.declare_parameter('interval', 30)
+        self.declare_parameter('scheduler', 'gcn')
+        nodes = self.get_parameter('nodes').get_parameter_value().string_array_value
+        interval = self.get_parameter('interval').get_parameter_value().integer_value
+        self.scheduler = self.get_parameter('scheduler').get_parameter_value().string_value
+
+        self.get_logger().info("SCHEDULER INIT")
+        self.graph = graph
+        self.interval = interval
+        self.all_nodes = nodes
+
+        self.current_schedule = {}
+        self.start_times = {}
+
+        self.executor_topics = {}
+        for node in nodes:
+            self.executor_topics[node] = self.create_publisher(String, f"/{node}/executor")
+
+        self.bandwidths: Dict[Tuple[str, str], float] = {}
+        for src, dst in product(nodes, nodes):
+            if src != dst:
+                self.create_subscription(
+                    Float64, f"/{src}/{dst}/delay",
+                    partial(self.delay_callback, src, dst)
+                )
+
+        self.create_subscription(String, "/output", self.output_callback)
+        self.makespan_publisher = self.create_publisher(Float64, "/makespan", 10)
+
+        self.graph_publisher: Publisher = self.create_publisher(Image, "/taskgraph")
+        self.create_timer(2, self.draw_task_graph)
+
+        self.current_tasks: Dict[str, str] = {}
+        for node in nodes:
+            self.create_subscription(
+                String, f"/{node}/current_task",
+                partial(self.current_task_callback, node)
+            )
+
+        self.create_timer(self.interval, self.execute)
+
+    #def get_bandwidth(self, n1: str, n2: str) -> float:
+    #    # avg = (self.bandwidths.get((n1,n2),0) + self.bandwidths.get((n2,n1),0)) / 2
+    #    # return (round(avg,2)
+    #    #         if self.bandwidths.get((n1,n2),0) != 0.0 and
+    #    #             self.bandwidths.get((n2,n1),0) != 0.0
+    #    #             else 1e-9
+    #    #             )
+    #    now = time.time()
+    #    ptime_1, bandwidth_1 = self.bandwidths.get((n1,n2), (0, 0))
+    #    ptime_2, bandwidth_2 = self.bandwidths.get((n2,n1), (0, 0))
+    #    if ptime_1 > ptime_2:
+    #        bandwidth = bandwidth_1 if (now - ptime_1) < 10 else 0
+    #    else:
+    #        bandwidth = bandwidth_2 if (now - ptime_2) < 10 else 0
+
+    #    return bandwidth + 1e-9
+
+    #def current_task_callback(self, node: str, msg: String) -> None:
+    #    self.current_tasks[node] = msg.data
+
+    #def output_callback(self, msg: String) -> None:
+    #    try:
+    #        msg_data = json.loads(msg.data)
+    #        makespan = Float64()
+    #        makespan.data = time.time() - self.start_times[msg_data["execution_id"]]
+    #        self.makespan_publisher.publish(makespan)
+    #    except:
+    #        self.get_logger().error(traceback.format_exc())
+
+    #def delay_callback(self, src: str, dst: str, msg: Float64) -> None:
+    #    bandwidth =  (BYTES_SENT/1000) / (msg.data/1000) if msg.data != 0.0 else 0.0
+    #    self.bandwidths[(src, dst)] = time.time(),bandwidth
+
+    #def get_schedule(self) -> Dict[str, str]:
+    #    try:
+    #        machines = [
+    #            node for node in self.all_nodes if not all([
+    #                self.get_bandwidth(node, other) <= 1e-3
+    #                for other in (set(self.all_nodes).difference({node}))
+    #            ])
+    #        ]
+    #        num_machines = len(machines)
+    #        if not num_machines:
+    #            return None
+
+    #        num_tasks = len(self.graph.task_names)
+    #        self.get_logger().info(f"{num_machines} machines")
+    #        self.get_logger().info(f"{num_tasks} tasks")
+    #        task_graph_ids = {
+    #            task: i
+    #            for i, task in enumerate(self.graph.task_names)
+    #        }
+    #        reverse_task_graph_ids = {v: k for k, v in task_graph_ids.items()}
+    #        task_graph = {
+    #            task_graph_ids[task.name]: [
+    #                task_graph_ids[dep.name]
+    #                for dep in deps
+    #            ]
+    #            for task, deps in self.graph.task_deps.items()
+    #        }
+    #        comm = torch.Tensor([
+    #            [
+    #                0 if i == j else min(1e9, 1 / self.get_bandwidth(machines[i], machines[j]))
+    #                for j in range(num_machines)
+    #            ]
+    #            for i in range(num_machines)
+    #        ])
+    #        self.get_logger().info("COMM:")
+    #        self.get_logger().info(str(comm))
+    #        comp = torch.Tensor([
+    #            [
+    #                self.graph.task_names[reverse_task_graph_ids[i]].cost
+    #                for i in range(num_tasks)
+    #            ]
+    #        ])
+    #        self.get_logger().info("COMP:")
+    #        self.get_logger().info(str(comp))
+    #        task_graph_forward: Dict[int, List[int]] = {}
+    #        for node_name, node_deps in task_graph.items():
+    #            for node_dep in node_deps:
+    #                task_graph_forward.setdefault(node_dep, [])
+    #                if node_name not in task_graph_forward[node_dep]:
+    #                    task_graph_forward[node_dep].append(node_name)
+    #        if self.scheduler == "gcn":
+    #            sched = find_schedule(
+    #                num_of_all_machines=num_machines,
+    #                num_node=num_tasks,
+    #                input_given_speed=torch.ones(1, num_machines),
+    #                input_given_comm=comm,
+    #                input_given_comp=comp,
+    #                input_given_task_graph=task_graph_forward
+    #            )
+    #            return {
+    #                reverse_task_graph_ids[task_i]: machines[node_i.item()]
+    #                for task_i, node_i in enumerate(sched)
+    #            }
+    #        else:
+    #            _, jobson = schedule_dag(
+    #                task_graph_forward,
+    #                agents=machines,
+    #                compcost=lambda task_id, agent: self.graph.task_names[reverse_task_graph_ids[task_id]].cost,
+    #                commcost=lambda t1, t2, a1, a2: 0 if a1 == a2 else 1 / self.get_bandwidth(a1, a2)
+    #            )
+    #            return {
+    #                reverse_task_graph_ids[task_id]: agent
+    #                for task_id, agent in jobson.items()
+    #            }
+    #    except:
+    #        self.get_logger().error(traceback.format_exc())
+    #    finally:
+    #        self.get_logger().debug("exiting get schedule function")
+
+    #def execute(self) -> Tuple[str, List[Future]]:
+    #    self.get_logger().info(f"\nSTARTING NEW EXECUTION")
+    #    schedule = self.get_schedule()
+    #    if not schedule:
+    #        return
+    #    self.current_schedule = deepcopy(schedule)
+    #    self.get_logger().info(f"SCHEDULE: {pformat(schedule)}")
+    #    execution_id = uuid4().hex
+    #    message = json.dumps({
+    #        "execution_id": execution_id,
+    #        "data": {},
+    #        "task_graph": self.graph.summary(),
+    #        "schedule": schedule
+    #    })
+
+    #    self.start_times[execution_id] = time.time()
+    #    for task in self.graph.start_tasks():
+    #        node = schedule[task]
+    #        msg = String()
+    #        msg.data = message
+    #        self.get_logger().info(f"SENDING INITIAL TASK {task} TO {node}")
+    #        self.executor_topics[node].publish(msg)
+
+    #async def draw_task_graph(self) -> None:
+    #    try:
+    #        start = time.time()
+    #        self.get_logger().info("Drawing taskgraph")
+
+    #        graph = nx.DiGraph()
+
+    #        for task in self.graph.task_deps:
+    #            for dep in self.graph.task_deps[task]:
+    #                graph.add_edge(dep.name,task.name)
+
+    #        self.get_logger().info(str(list(graph.nodes)))
+    #        types = {
+    #            t: i for i, t in enumerate(self.all_nodes)
+    #        }
+    #        self.get_logger().info("types "+str(types))
+    #        self.get_logger().info("nodes "+str(self.all_nodes))
+
+    #        current_schedule = deepcopy(self.current_schedule)
+    #        {node: task for task, node in current_schedule.items()}
+    #        try:
+    #            node_color = [
+    #                types[current_schedule[node]] for node in graph.nodes
+    #            ]
+    #        except:
+    #            return
+    #        self.get_logger().info("color "+str(node_color))
+    #        cmap = cm.get_cmap('gist_rainbow', len(self.all_nodes))
+    #        labels_dict = dict([(node_name, node_name.split("_")[0][:7]) for node_name in graph.nodes])
+
+    #        pos = nx.nx_agraph.graphviz_layout(graph,prog='dot')
+    #        fig = plt.figure()
+    #        nx.draw(
+    #            graph, pos, edge_color='black', width=1, linewidths=1,
+    #            node_size=200, node_color=node_color, alpha=0.8,
+    #            labels=labels_dict,
+    #            with_labels = False,
+    #            font_weight = 'bold',
+    #            font_size=8,
+    #            cmap=cmap,vmin=0,vmax=len(self.all_nodes)
+    #        )
+    #        color_lines = [mpatches.Patch(color=cmap(types[t]), label=t) for t in types.keys()]
+    #        legend = plt.legend(handles=color_lines, loc='best')
+    #        # nx.draw_networkx_edge_labels(
+    #        #     graph, pos,
+    #        #     edge_labels=bandwidths,
+    #        #     font_color='red'
+    #        # )
+
+    #        fig.canvas.draw()
+
+    #        # convert canvas to image
+    #        img = np.fromstring(fig.canvas.tostring_rgb(), dtype=np.uint8, sep='')
+    #        img  = img.reshape(fig.canvas.get_width_height()[::-1] + (3,))
+    #        # self.get_logger().info(str(img))
+
+    #        bridge = CvBridge()
+    #        img_msg = Image()
+    #        # img_msg.header.stamp = self.get_clock().now()
+    #        img_msg = bridge.cv2_to_imgmsg(img, encoding="rgb8")
+
+    #        self.graph_publisher.publish(img_msg)
+    #        self.get_logger().info(f"publishing task graph image for visualization {time.time()-start} seconds")
+    #        plt.close(fig)
+    #    except:
+    #        self.get_logger().error(traceback.format_exc())
+    #    finally:
+    #        self.get_logger().info(f"leaving task graph drawer")
+
+def main(args=None):
+#    rclpy.init(args=args)
+#
+#    all_nodes = []
+#
+#    gcn_sched = Scheduler(
+#        nodes=all_nodes,
+#        graph=get_graph(),
+#        interval=10
+#    )
+#
+#    try:
+#        rclpy.spin(gcn_sched)
+#    finally:
+#        gcn_sched.destroy_node()
+#        rclpy.shutdown()
+#
+    g = get_graph()
+    num_tasks = len(g.task_names)
+    #self.get_logger().info(f"{num_machines} machines")
+    #self.get_logger().info(f"{num_tasks} tasks")
+    task_graph_ids = {
+                task: i
+                for i, task in enumerate(g.task_names)
+            }
+    reverse_task_graph_ids = {v: k for k, v in task_graph_ids.items()}
+    task_graph = {
+                    task_graph_ids[task.name]: [
+                    task_graph_ids[dep.name]
+                    for dep in deps
+                ]
+                for task, deps in g.task_deps.items()
+            }
+           # comm = torch.Tensor([
+           #     [
+           #         0 if i == j else min(1e9, 1 / self.get_bandwidth(machines[i], machines[j]))
+           #         for j in range(num_machines)
+           #     ]
+           #     for i in range(num_machines)
+           # ])
+            #self.get_logger().info("COMM:")
+            #self.get_logger().info(str(comm))
+    comp = torch.Tensor([
+                [
+                    g.task_names[reverse_task_graph_ids[i]].cost
+                    for i in range(num_tasks)
+                ]
+            ])
+            #self.get_logger().info("COMP:")
+            #self.get_logger().info(str(comp))
+    task_graph_forward: Dict[int, List[int]] = {}
+    for node_name, node_deps in task_graph.items():
+        for node_dep in node_deps:
+            task_graph_forward.setdefault(node_dep, [])
+            if node_name not in task_graph_forward[node_dep]:
+                task_graph_forward[node_dep].append(node_name)
+    print("task graph",task_graph_forward)
+    print("comp",comp)
+if __name__ == '__main__':
+    main()
